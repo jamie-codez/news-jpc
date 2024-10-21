2416b612b0208b18ee0b099ba32eda33b4039181
jamie-codez <cruiseomondi90@gmail.com>
2024-10-21 03:06:13 +0300

    Uwii nafaa viboko -> Forgot API KEY in code

--- app/src/main/java/com/jamie/newsapp/util/Constants.kt
+++ app/src/main/java/com/jamie/newsapp/util/Constants.kt
@@ -3,6 +3,6 @@ package com.jamie.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
-    const val API_KEY = "2bf28394f10e46838006d51d7c9dd336"
+    const val API_KEY = ""
     const val BASE_URL = "https://newsapi.org/v2/"
 }
